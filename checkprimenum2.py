9e12b75bc6daae79e8b64a64538e1422b4023455
Justin Kim <neibc96@gmail.com>
2022-07-17 17:00:48 +0900

    Update checkprimenum2.py
    add continuous input mode

--- checkprimenum2.py
+++ checkprimenum2.py
@@ -16,7 +16,7 @@ def is_prime(n):
     if n % 2 == 0 and n > 2: 
         return False
     return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
-	
+        
 def divisorGenerator(n):
     large_divisors = []
     for i in range(1, int(math.sqrt(n) + 1)):
@@ -32,25 +32,27 @@ argcnt = len(sys.argv)
 print('usage : ex> python checkprimenum.py [ispause]\n')
 if argcnt > 1:
     ispause = 1
-	
+        
 print('environment_uname : ', end='')
 print(platform.platform())
-number = int(input("input value(>1) ex> 2020123212321223 : "))
-print('-------------------------------------------------------')
-print('start time : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
-starta = datetime.now()
+number = 1
+while number > 0:
+    number = int(input("input value(>1, 0 to end) ex> 2020123212321223 : "))
+    print('-------------------------------------------------------')
+    print('start time : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
+    starta = datetime.now()
 
-print('\nresult     : ', end='')
-print(is_prime(number))
+    print('\nresult     : ', end='')
+    print(is_prime(number))
 
-print('\nresult     : ', end='')
-print(list(divisorGenerator(number)))
+    print('\nresult     : ', end='')
+    print(list(divisorGenerator(number)))
 
-startb = datetime.now()
-print('\nend time   : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
-print('-------------------------------------------------------')
-print('time diff  : ', end='')
-print(startb-starta)
+    startb = datetime.now()
+    print('\nend time   : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
+    print('-------------------------------------------------------')
+    print('time diff  : ', end='')
+    print(startb-starta)
 
 if ispause > 0:
     input('enter key to end')
