5db310a55480bceeb879e6c0434f66b5cbd94756
Justin Kim <neibc96@gmail.com>
2025-06-15 17:27:49 +0900

    Refactor checkprimenum and add unit tests

--- checkprimenum.py
+++ checkprimenum.py
@@ -9,35 +9,41 @@ from datetime import datetime
 import sys
 import platform
 
-#global variables
-ispause = 0
 
 def is_prime(n):
-    if n % 2 == 0 and n > 2: 
+    """Return True if *n* is a prime number."""
+    if n % 2 == 0 and n > 2:
         return False
     return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
-argcnt = len(sys.argv)
+def main():
+    """Run a simple command line interface for checking primality."""
+    argcnt = len(sys.argv)
+    ispause = 0
 
-print('usage : ex> python checkprimenum.py [ispause]\n')
-if argcnt > 1:
-    ispause = 1
-	
-print('environment_uname : ', end='')
-print(platform.platform())
-number = int(input("input value(>1) ex> 2020123212321223 : "))
-print('-------------------------------------------------------')
-print('start time : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
-starta = datetime.now()
+    print('usage : ex> python checkprimenum.py [ispause]\n')
+    if argcnt > 1:
+        ispause = 1
 
-print('\nresult     : ', end='')
-print(is_prime(number))
+    print('environment_uname : ', end='')
+    print(platform.platform())
+    number = int(input("input value(>1) ex> 2020123212321223 : "))
+    print('-------------------------------------------------------')
+    print('start time : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
+    starta = datetime.now()
 
-startb = datetime.now()
-print('\nend time   : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
-print('-------------------------------------------------------')
-print('time diff  : ', end='')
-print(startb-starta)
+    print('\nresult     : ', end='')
+    print(is_prime(number))
 
-if ispause > 0:
-    input('enter key to end')
+    startb = datetime.now()
+    print('\nend time   : ' + datetime.utcnow().strftime('%Y/%m/%d_%H:%M:%S.%f'))
+    print('-------------------------------------------------------')
+    print('time diff  : ', end='')
+    print(startb-starta)
+
+    if ispause > 0:
+        input('enter key to end')
+
+
+if __name__ == '__main__':
+    main()
